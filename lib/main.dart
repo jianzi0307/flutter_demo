cff6d96922d71a81dc37f753f121ef8541b53a80
jianzi0307 <jianzi0307@qq.com>
2019-02-26 00:53:24 +0800

    listview -> gridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,44 @@
 import 'package:flutter/material.dart';
-import 'demo/app.dart';
+import 'demo/pages/index.dart';
 
 void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const Map<String, String> _gridsItems = {
+      'text': '文本',
+      'button': '按钮',
+      'image': '图片',
+      'icon': 'IconFont',
+      'switch': '单选框和复选框',
+      'input': '输入框和表单',
+      'qrcode': '二维码',
+      'scaffold': '脚手架',
+      'network': '网络'
+    };
+
+    return MaterialApp(
+      title: 'Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: DemoHome(
+        title: 'Demo',
+        gridItems: _gridsItems,
+      ),
+      routes: {
+        'text_route': (context) => TextRoute(),
+        'button_route': (context) => ButtonRoute(),
+        'image_route': (context) => ImageRoute(),
+        'icon_route': (context) => IconRoute(),
+        'switch_route': (context) => SwitchRoute(),
+        'input_route': (context) => InputRoute(),
+        'qrcode_route': (context) => QrcodeRoute(),
+        'form_route': (context) => FormRoute(),
+        'scaffold_route': (context) => ScaffoldRoute(),
+        'network_route': (context) => NetworkRoute()
+      },
+    );
+  }
+}
