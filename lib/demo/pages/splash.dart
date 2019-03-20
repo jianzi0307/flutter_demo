0626c75740c8530f8a62408d4a5723971f8f6ec1
jianzi0307 <jianzi0307@qq.com>
2019-03-20 23:42:45 +0800

    test

--- lib/demo/pages/splash.dart
+++ lib/demo/pages/splash.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SplashRoute extends StatefulWidget {
-  final Widget child;
-
-  SplashRoute({Key key, this.child}) : super(key: key);
+  SplashRoute({Key key}) : super(key: key);
 
   _SplashRouteState createState() => _SplashRouteState();
 }
@@ -11,8 +9,6 @@ class SplashRoute extends StatefulWidget {
 class _SplashRouteState extends State<SplashRoute> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: widget.child,
-    );
+    return Container(child: Text('测试'));
   }
 }
